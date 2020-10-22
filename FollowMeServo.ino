4bc611085b592ae626dde23c34cc856d1ce5306d
andysheen <andymsheen@gmail.com>
2020-10-22 11:33:48 +0100

    minor changed
    - moved pot and servo to same side of esp32s board
    - made it so pot can be plugged directly into board without wires
    - decresed the threshold
    -decreased the tape size to 5 seconds

--- FollowMeServo.ino
+++ FollowMeServo.ino
@@ -4,13 +4,13 @@ Servo myservo;  // create servo object to control a servo
 // 16 servo objects can be created on the ESP32
 
 // Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33
-const int servoPin = 18;
-const int potPin = 34;
+const int servoPin = 13;
+const int potPin = 25;
 
 int updateIntervalMs = 100;
 long updateAtMs = 0;
 
-#define TAPE_SIZE 100
+#define TAPE_SIZE 50
 int tape[TAPE_SIZE];
 int tapeHeadPos = 0;
 
@@ -22,14 +22,17 @@ typedef enum {
 State currentState = INACTIVE;
 
 int lastPotReading = 0;
-int potReadingChangedThreshold = 50;
+int potReadingChangedThreshold = 10;
 
 long lastChangeAtMs = 0;
 int noChangeRecordingTimeoutMS = 1000;
 
 void setup() {
-  Serial.begin(9600);
-
+  Serial.begin(115200);
+  pinMode(26, OUTPUT);
+  digitalWrite(26, 1);
+  pinMode(33, OUTPUT);
+  digitalWrite(33, 0);
   // Allow allocation of all timers
   ESP32PWM::allocateTimer(0);
   ESP32PWM::allocateTimer(1);
@@ -44,12 +47,14 @@ void setup() {
 
 void loop() {
   if (millis() > updateAtMs) {
+    Serial.println(analogRead(potPin));
     updateAtMs = millis() + updateIntervalMs;
-    
-    switch(currentState) {
+
+    switch (currentState) {
       case RECORD:
         Serial.println("RECORD");
         tape[tapeHeadPos] = analogRead(potPin);
+        Serial.println(tape[tapeHeadPos]);
         myservo.write(map(tape[tapeHeadPos], 0, 4095, 0, 180));
         break;
       case PLAY:
@@ -66,14 +71,14 @@ void loop() {
   }
 }
 
-void updateState() { 
+void updateState() {
   int potReading = analogRead(potPin);
-  
-  if(abs(potReading-lastPotReading) > potReadingChangedThreshold) {
+
+  if (abs(potReading - lastPotReading) > potReadingChangedThreshold) {
     //Serial.println("CHANGE");
     lastChangeAtMs = millis();
 
-    switch(currentState) {
+    switch (currentState) {
       case RECORD:    break;
       case PLAY:
         currentState = RECORD;
@@ -83,9 +88,9 @@ void updateState() {
         break;
     }
   }
-  else if((millis() - lastChangeAtMs) > noChangeRecordingTimeoutMS) {
+  else if ((millis() - lastChangeAtMs) > noChangeRecordingTimeoutMS) {
     //Serial.println("TIMEOUT");
-    switch(currentState) {
+    switch (currentState) {
       case RECORD:
         currentState = PLAY;
         break;
